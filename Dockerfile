2c7684e8420e12248428d87493378e6455386387
Max Bigras <mbigras22@gmail.com>
2020-10-24 01:05:35 -0700

    Run uwsgi server as nobody user

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,10 @@
 FROM python:3.7.9
+
+RUN apt-get update && apt-get install -y gosu
+
 WORKDIR /app
 COPY requirements.txt ./
 RUN pip install -r requirements.txt
 COPY version.py app.py wsgi.py entrypoint.sh slack.sh ./
+
 ENTRYPOINT ["/app/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-# Start uWSGI server.
+# Start uWSGI server as the nobody user.
 
 PORT=${PORT:-3000}
 
-exec uwsgi --protocol=http --wsgi=wsgi:app --socket=0.0.0.0:$PORT $@
+exec gosu nobody uwsgi --socket=0.0.0.0:$PORT --wsgi=wsgi:app --protocol=http --master
